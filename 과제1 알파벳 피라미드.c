867ad39d9b688b120f577a22015d5f5a9d83cd30
kangg <kanggavin@naver.com>
2025-12-23 16:09:42 +0900

    day1

---
+++ 과제1 알파벳 피라미드.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("\t\tA\t\t\n\tB\t\tC\t\nD\t\tE\t\tF\n\tG\t\tH\n\t\tT\t\t");
+	return 0;
+}
+//             \t의 탭의 범위를 아는 문제
+//저렇게 한라인말고 
+// printf 한줄씩 해서 사용가능 
+
+
+// 이렇게도 사용가능 
+
+//int main(void)
+//{
+//	printf("\t\tA\t\t\n");
+//	printf("\tB\t\tC\t\n");
+//	printf("D\t\tE\t\tF\n");
+//	printf("\tG\t\tH\n");
+//	printf("\t\tI\n");
+//
+//	return 0;
+//}
---
+++ 기본구조.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	printf("Hello world!\n");
+	printf("오늘은 월요일 입니다\n");
+	printf("가\n나\n다\n라\n\n마\n바\n");
+	printf("한글\t영어\t불어\n");//n은 new line t는 tap 
+	printf("kr\teng\tfr\n");
+	printf("12345678\t1234\n");
+	printf("강아지는 \"멍멍\"고양이는 \"야옹\" 토끼는 \"깡총\"");//""텍스트로 쓰고싶으면 앞에 \붙이기
+	return 0;
+}
